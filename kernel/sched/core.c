1bc5947f8fa18c2ac335d53f7a80bf3708403bb4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-04-09 23:50:35 +0800

    Revert "kernel: Only expose su when daemon is running"
    This reverts commit 1655eae6c484f02566c60e388dab3b86251ed66a.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -95,37 +95,28 @@
 #include "walt.h"
 #include "tune.h"
 
-static atomic_t __su_instances;
+const char *task_event_names[] = {"PUT_PREV_TASK", "PICK_NEXT_TASK",
+				  "TASK_WAKE", "TASK_MIGRATE", "TASK_UPDATE",
+				"IRQ_UPDATE"};
 
-int su_instances(void)
-{
-	return atomic_read(&__su_instances);
-}
+ATOMIC_NOTIFIER_HEAD(migration_notifier_head);
+ATOMIC_NOTIFIER_HEAD(load_alert_notifier_head);
 
-bool su_running(void)
+#ifdef smp_mb__before_atomic
+void __smp_mb__before_atomic(void)
 {
-	return su_instances() > 0;
+	smp_mb__before_atomic();
 }
+EXPORT_SYMBOL(__smp_mb__before_atomic);
+#endif
 
-bool su_visible(void)
+#ifdef smp_mb__after_atomic
+void __smp_mb__after_atomic(void)
 {
-	kuid_t uid = current_uid();
-	if (su_running())
-		return true;
-	if (uid_eq(uid, GLOBAL_ROOT_UID) || uid_eq(uid, GLOBAL_SYSTEM_UID))
-		return true;
-	return false;
+	smp_mb__after_atomic();
 }
-
-void su_exec(void)
-{
-	atomic_inc(&__su_instances);
-}
-
-void su_exit(void)
-{
-	atomic_dec(&__su_instances);
-}
+EXPORT_SYMBOL(__smp_mb__after_atomic);
+#endif
 
 void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 {
