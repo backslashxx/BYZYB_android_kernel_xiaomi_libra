fb5e471ce3960fa478705c3181161c2ec82a93d9
Derek <nielson925@gmail.com>
2025-04-26 12:15:44 +0800

    BACKPORT ext4 crypto: add Adiantum support
    Backport of commit e33fa31e300 in the android-4.9-q
    kernel which adds adiantum support to fscrypt
    
    Change-Id: Id63e3f5b4bfe0b411dd0ce18981b67f52705a0df

--- fs/ext4/crypto_policy.c
+++ fs/ext4/crypto_policy.c
@@ -185,7 +185,7 @@ int ext4_is_child_context_consistent_with_parent(struct inode *parent,
 	parent_ci = EXT4_I(parent)->i_crypt_info;
 	child_ci = EXT4_I(child)->i_crypt_info;
 	if (parent_ci && child_ci) {
-		return memcmp(parent_ci->ci_master_key, child_ci->ci_master_key,
+		return memcmp(parent_ci->ci_master_key_descriptor, child_ci->ci_master_key_descriptor,
 			      EXT4_KEY_DESCRIPTOR_SIZE) == 0 &&
 			(parent_ci->ci_data_mode == child_ci->ci_data_mode) &&
 			(parent_ci->ci_filename_mode ==
@@ -248,7 +248,7 @@ int ext4_inherit_context(struct inode *parent, struct inode *child)
 		ctx.contents_encryption_mode = ci->ci_data_mode;
 		ctx.filenames_encryption_mode = ci->ci_filename_mode;
 		ctx.flags = ci->ci_flags;
-		memcpy(ctx.master_key_descriptor, ci->ci_master_key,
+		memcpy(ctx.master_key_descriptor, ci->ci_master_key_descriptor,
 		       EXT4_KEY_DESCRIPTOR_SIZE);
 	}
 	get_random_bytes(ctx.nonce, EXT4_KEY_DERIVATION_NONCE_SIZE);
