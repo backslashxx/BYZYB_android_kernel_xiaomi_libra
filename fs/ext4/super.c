556563e1ada8f4d57e5a4efe00bb644a891ae865
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-16 21:20:55 +0800

    fs: ext4: ext4_unregister_sysfs also remove nodes due to backports

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -2561,6 +2561,8 @@ void ext4_unregister_sysfs(struct super_block *sb)
 	// copied from ext4_fill_super(), failed_mount
 	struct ext4_sb_info *sbi = EXT4_SB(sb);
 	if (sbi->s_proc) {
+		remove_proc_entry("es_shrinker_info", sbi->s_proc);
+		remove_proc_entry("mb_groups", sbi->s_proc);
 		remove_proc_entry("options", sbi->s_proc);
 		remove_proc_entry(sb->s_id, ext4_proc_root);
 	}
