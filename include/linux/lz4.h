2950c1e7440b55d44ff0fb5dc55a78c8ac487146
Mandi-Sa <799620521@qq.com>
2026-07-21 00:28:54 +0800

    lib/lz4: Import arm64 V8 ASM lz4 decompression acceleration
    Change-Id: I3c8dd91df090bb692784a6b7a61c8877b1e1dfba

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -253,4 +253,7 @@ int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
 int LZ4_compress_HC(const char *src, char *dst, int srcSize, int dstCapacity,
 	int compressionLevel, void *wrkmem);
 
+ssize_t LZ4_arm64_decompress_safe(const void *source, void *dest, size_t inputSize, size_t outputSize, bool dip);
+ssize_t LZ4_arm64_decompress_safe_partial(const void *source, void *dest, size_t inputSize, size_t outputSize, bool dip);
+
 #endif
