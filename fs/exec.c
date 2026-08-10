4853314b6032a891d9b1c474dcf29bc14ea700d2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-08-10 17:58:28 +0800

    do_execve_common: mark as used
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1514,6 +1514,7 @@ EXPORT_SYMBOL(search_binary_handler);
 /*
  * sys_execve() executes a new program.
  */
+__attribute__((used))
 static int do_execve_common(const char *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp)
